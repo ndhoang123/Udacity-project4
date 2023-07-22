8638f4752a786852d7f3f72857a8219fde3b59b3
Hoang Nguyen Dang <hoangnguyen12397@gmail.com>
2023-07-22 15:56:16 +0700

    Update some scripts

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=sergionguyen12397/project4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag project4 $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
